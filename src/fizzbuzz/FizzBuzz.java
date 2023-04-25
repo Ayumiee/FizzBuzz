c920e8027efb07c43ab875911397ea0f7d71d500
Mathilde Darques <mathildedarques.m@gmail.com>
2023-04-26 00:29:57 +0200

    ex6

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -6,29 +6,61 @@ import java.util.Arrays;
 public class FizzBuzz {
 
 	public static Object fizzbuzz(String string) {
-		// 2 personnes et ++
 		String resultat = "Hello, ";
 		ArrayList<String> tabString = new ArrayList<>(Arrays.asList(string.split(",")));
+		ArrayList<String> tabStringMaj = new ArrayList<String>();
+		ArrayList<String> tabStringMin = new ArrayList<String>();
+
+		// si 2+ personnes :
 		if (tabString.size() > 1) {
+			// tri des éléments majuscules et minuscules
 			for (int i = 0; i < tabString.size(); i++) {
-				resultat = resultat + tabString.get(i).substring(0, 1).toUpperCase() + tabString.get(i).substring(1);
-				if (i + 1 < tabString.size()) {
+				if (tabString.get(i).toUpperCase() == tabString.get(i)) {
+					tabStringMaj.add(tabString.get(i));
+				} else {
+					tabStringMin.add(tabString.get(i));
+				}
+			}
+			// traitement des potentiels elements minuscules trouvés
+			for (int i = 0; i < tabStringMin.size(); i++) {
+				resultat = resultat + tabStringMin.get(i).substring(0, 1).toUpperCase()
+						+ tabStringMin.get(i).substring(1);
+				if (i + 1 < tabStringMin.size()) {
 					resultat = resultat + ", ";
 				}
 			}
-			return resultat;
-		}
 
-		// nom vide
-		if (string.isBlank()) {
-			return "Hello, my friend";
-		}
-		// tout en majuscule
-		if (string.toUpperCase() == string) {
-			return "HELLO, " + string + " !";
+			// traitement des potentiels elements majuscules trouvés
+			if (tabStringMaj.size() > 0) {
+				if (tabStringMin.size() > 0) {
+					resultat = resultat + ". AND HELLO, ";
+				} else {
+					resultat = "HELLO, ";
+				}
+				for (int i = 0; i < tabStringMaj.size(); i++) {
+					resultat = resultat + tabStringMaj.get(i);
+					if (i + 1 < tabStringMaj.size()) {
+						resultat = resultat + ", ";
+					} else {
+						resultat = resultat + " !";
+					}
+				}
+			}
+			return resultat;
+
+			// si 1 personne :
 		} else {
-			// premi�re lettre en maj
-			return "Hello, " + string.substring(0, 1).toUpperCase() + string.substring(1);
+			// nom vide
+			if (string.isBlank()) {
+				return "Hello, my friend";
+			}
+			// tout en majuscule
+			if (string.toUpperCase() == string) {
+				return "HELLO, " + string + " !";
+			} else {
+				// premiere lettre en maj
+				return "Hello, " + string.substring(0, 1).toUpperCase() + string.substring(1);
+			}
 		}
 	}
 
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -31,5 +31,15 @@ class FizzBuzzTest {
 	void ex5() {
 		assertEquals(FizzBuzz.fizzbuzz("amy,bob,jerry"), "Hello, Amy, Bob, Jerry");
 	}
+	
+	@Test
+	void ex6() {
+		assertEquals(FizzBuzz.fizzbuzz("Amy,BOB,Jerry"), "Hello, Amy, Jerry. AND HELLO, BOB !");
+		assertEquals(FizzBuzz.fizzbuzz("Amy,BOB,Jerry,MARK"), "Hello, Amy, Jerry. AND HELLO, BOB, MARK !");
+		assertEquals(FizzBuzz.fizzbuzz("Amy,BOB,Jerry,MARK,MALA"), "Hello, Amy, Jerry. AND HELLO, BOB, MARK, MALA !");
+		assertEquals(FizzBuzz.fizzbuzz("Amy,BOB,Jerry,MARK,MALA,Pedro"), "Hello, Amy, Jerry, Pedro. AND HELLO, BOB, MARK, MALA !");
+		assertEquals(FizzBuzz.fizzbuzz("BOB,MARK"), "HELLO, BOB, MARK !");
+		
+	}
 
 }
